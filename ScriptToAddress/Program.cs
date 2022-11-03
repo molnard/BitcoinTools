39b6ae531b4c744f0b5871a50cce9ac2aef68802
David Molnar <molnardavid84@gmail.com>
2022-11-03 11:20:46 +0100

    Add iteration translate

--- ScriptToAddress/Program.cs
+++ ScriptToAddress/Program.cs
@@ -1,10 +1,14 @@
 ﻿using NBitcoin;
 
-Console.WriteLine("Give me your scriptPubKey and press ENTER.");
-var scriptPubKeyText = Console.ReadLine();
-Script scriptPubKey = new (scriptPubKeyText);
+do
+{ 
+    Console.WriteLine("Give me your scriptPubKey HEX and press ENTER.");
+    var scriptPubKeyText = Console.ReadLine();
+    Script scriptPubKey = Script.FromHex(scriptPubKeyText);
 
-Console.WriteLine($"Here is your address - press ENTER.");
-var address = scriptPubKey.GetDestinationAddress(Network.Main);
-Console.WriteLine(address.ToString());
-Console.ReadLine();
+    Console.WriteLine($"Here is your address - press ENTER.");
+    var address = scriptPubKey.GetDestinationAddress(Network.Main);
+    Console.WriteLine(address.ToString());
+}
+while(true);
+
