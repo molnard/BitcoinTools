5d9545843802b12db785f23af1eb11e7526bfa0e
David Molnar <molnardavid84@gmail.com>
2022-11-03 11:15:11 +0100

    Add scriptToAddress

---
+++ ScriptToAddress/Program.cs
@@ -0,0 +1,10 @@
+﻿using NBitcoin;
+
+Console.WriteLine("Give me your scriptPubKey and press ENTER.");
+var scriptPubKeyText = Console.ReadLine();
+Script scriptPubKey = new (scriptPubKeyText);
+
+Console.WriteLine($"Here is your address - press ENTER.");
+var address = scriptPubKey.GetDestinationAddress(Network.Main);
+Console.WriteLine(address.ToString());
+Console.ReadLine();
